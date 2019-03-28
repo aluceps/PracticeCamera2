fbd522e674f5d772d4e27f5187a8141e4189385a
Atsushi Takano <aluceps@gmail.com>
2019-03-28 21:56:56 +0900

    first commit

---
+++ app/src/main/java/me/aluceps/practicecamera2/MainActivity.kt
@@ -0,0 +1,18 @@
+package me.aluceps.practicecamera2
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.aluceps.practicecamera2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding
+    }
+}
