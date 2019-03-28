764c7d713c5edc850abd61851b61ea8b312025b0
Atsushi Takano <aluceps@gmail.com>
2019-03-28 22:05:41 +0900

    Hide statusbar.

--- app/src/main/java/me/aluceps/practicecamera2/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicecamera2/MainActivity.kt
@@ -3,6 +3,7 @@ package me.aluceps.practicecamera2
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import me.aluceps.practicecamera2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding
+    }
+
+    override fun onStart() {
+        super.onStart()
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_IMMERSIVE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
